173243c05c346ddc091d9872bd5fdad6391ab2cd
Archi <01fe22bca222@kletech.ac.in>
2024-02-14 17:30:38 +0530

    Updated the code for Extended Airthmatic Operations

---
+++ extendedcalculator.java
@@ -0,0 +1,15 @@
+package Calculator;
+public class extendedcalculator {
+
+    int Triangle(int base,int height)
+    {
+        int area =(1/2*base*height);
+        return area;
+    }
+    public static float Modulas (float op1 , float op2)
+    {
+        return op1 % op2;
+    }
+
+    
+}
